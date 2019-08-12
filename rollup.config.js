969633ad748504b2088b9157176a45997b81395d
Petr Mitev <p.mitevpi@gmail.com>
2019-08-12 10:59:19 -0400

    remove uglify, can't compile es6

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
+// import { uglify } from "rollup-plugin-uglify";
 module.exports = {
-  input: "src/index.js",
-  output: {
-    // file: "bundle.js",
-    format: "cjs"
-  }
+  input: "dist/index.js"
+  // plugins: [uglify()]
+  // output: {
+  //   // file: "bundle.js",
+  //   format: "umd"
+  // }
 };
